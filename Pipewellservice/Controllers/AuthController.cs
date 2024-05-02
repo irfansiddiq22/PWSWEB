de3def8c3d86e24e47a39bf6cd1de8cd91e131b9
Irfan Siddiq <irfan_siddiq_21@yahoo.com>
2024-05-02 09:57:32 +0500

    GMAIL SMTP

--- Pipewellservice/Controllers/AuthController.cs
+++ Pipewellservice/Controllers/AuthController.cs
@@ -28,7 +28,7 @@ namespace Pipewellservice.Controllers
             }
             return new JsonResult
             {
-                Data = (result.ID>0),
+                Data = (result.ID > 0),
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
 
@@ -46,16 +46,28 @@ namespace Pipewellservice.Controllers
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
         }
+        public async Task<JsonResult> TestOTP(string EmailAddress)
+        {
+
+            EmailHelper email = new EmailHelper();
+          var status=  await email.SendEmail(new EmailDTO() { To = EmailAddress, From = "no-reply@pipewellservices.com", Subject = "PWS WEB TEST", Body = "TEST email server" });
+            return new JsonResult
+            {
+                Data = status,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+
+        }
         public async Task<JsonResult> GenerateOTP(string EmailAddress)
         {
             string OTPPassword = StringHelper.GenerateOTP();
-            var result = await json.SaveOTP(new OTP() {EmailAddress=EmailAddress,OTPPassword= OTPPassword });
+            var result = await json.SaveOTP(new OTP() { EmailAddress = EmailAddress, OTPPassword = OTPPassword });
             var OTPSuccess = false;
-            if (result!="")
+            if (result != "")
             {
                 OTPSuccess = true;
                 EmailHelper email = new EmailHelper();
-                await email.SendEmail(new EmailDTO() { To = EmailAddress, From = "no-reply@pipewellservices.com", Subject="OTP to accees PWS WEB", Body=$"Dear{result}<br/><br/> Use this OPT to access PWS web panel {OTPPassword}. This OPT will expire in next 10 minutes. <br> Ignore this email if you did not generate this OTP. <br/> thanks " });
+                await email.SendEmail(new EmailDTO() { To = EmailAddress, From = "notifications.pws@gmail.com", Subject = "OTP to accees PWS WEB", Body = $"Dear {result}. <br/><br/> Use this OPT to access PWS web panel {OTPPassword}. This OPT will expire in next 10 minutes. <br> Ignore this email if you did not generate this OTP. <br/> thanks. " });
             }
             return new JsonResult
             {
--- PipewellserviceModels/Common/EmailHelper.cs
+++ PipewellserviceModels/Common/EmailHelper.cs
@@ -13,15 +13,17 @@ namespace PipewellserviceModels.Common
         public async Task<bool> SendEmail(EmailDTO email)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            MailMessage mail = new MailMessage(email.From, email.To, email.Subject, email.Body);
-            SmtpClient smtpServer = new SmtpClient("smtp.office365.com");
-            smtpServer.Port = 587; // Port for SMTP
+            MailMessage mail = new MailMessage("notifications.pws@gmail.com", email.To, email.Subject, email.Body);
+            mail.IsBodyHtml = true;
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com", 587);
+            //smtpServer.Port = 587; // Port for SMTP
 
             // Enable SSL
-            smtpServer.EnableSsl = true;
-
+            smtpServer.EnableSsl =  true;
+            smtpServer.UseDefaultCredentials = false;
             // Set the credentials
-            smtpServer.Credentials = new NetworkCredential("no-reply@pipewellservices.com", "Tof28006");
+            smtpServer.Credentials = new NetworkCredential("notifications.pws@gmail.com", "qqtfxhawakolbput");
+            
 
             try
             {
@@ -31,7 +33,7 @@ namespace PipewellserviceModels.Common
             }
             catch (Exception ex)
             {
-                return false;
+                throw ex;
             }
 
         }
